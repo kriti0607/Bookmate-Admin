d1e355a38ce88d7ebabd4f8cfbc1d3cefe927e5e
dchoudhary0 <dilpreetchoudhary0@gmail.com>
2025-07-04 18:20:52 +0530

    login page transitions to tsx

---
+++ src/LoginPage/LoginPage.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./LoginPage.css";
+
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (username && password) {
+      navigate("/home");
+    } else {
+      alert("Please enter both username and password.");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <img src= "/abc.png" alt="Bookmate Logo" className="logo" />
+
+      <div className="login-form">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          className="input-field"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          autoComplete="username"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input-field"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <button className="login-button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
